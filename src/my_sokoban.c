f01255f236df5f2a9b4cea8fb8d6381044ce5e1c
Herosbrine <gregoire.bezier@epitech.eu>
2019-12-05 18:30:36 +0100

    [ADD] | space restart map

--- src/my_sokoban.c
+++ src/my_sokoban.c
@@ -9,25 +9,36 @@
 #include <stdlib.h>
 #include "my.h"
 
+void print_map(char *map, int y, int x, int *ch)
+{
+        mvprintw(0, 0, "%s", map);
+        mvprintw(y, x, "P");
+        refresh();
+        *ch = getch();
+}
+
+void checker(int a, int *o_position, char *map)
+{
+    check_o(a, o_position, map);
+    if (check_end(a, o_position, map) == 1) {
+            endwin();
+            exit (0);
+    }
+}
+
 char game_loop(int x, char *map)
 {
-    int y = 0;
-    int cp = count_p(map);
-    int *p_position = position_p(cp, map);
-    int a = count_o(map);
-    int *o_position = position_o(a, map);
-    int r = count_box(map);
-    int *box_position = position_box(r, map);
+    int y = 0, cp = count_p(map), *p_position = position_p(cp, map);
+    int ch = 0, a = count_o(map),  *o_position = position_o(a, map);
+    int r = count_box(map),  *box_position = position_box(r, map);
 
     x = find_p(map);
     y = count_nbr_y(y, map);
     while (1) {
-        check_o(a, o_position, map);
-        mvprintw(0, 0, "%s", map);
-        mvprintw(y, x, "P");
-        refresh();
-        pos_up(&x, &y, getch(), map);
-        if (check_end(a, o_position, map) == 1) {
+        checker(a, o_position, map);
+        print_map(map, y, x, &ch);
+        pos_up(&x, &y, ch, map);
+        if (ch == ' ') {
             put_p_again(cp, p_position, map);
             erase_pre_box(map);
             put_box_again(r, box_position, map);
