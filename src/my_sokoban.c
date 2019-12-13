b21040c79a255caa4654695cf6ef617d1bcc6061
Herosbrine <gregoire.bezier@epitech.eu>
2019-12-13 14:52:27 +0100

    [ADD] | win condition

--- src/my_sokoban.c
+++ src/my_sokoban.c
@@ -9,12 +9,11 @@
 #include <stdlib.h>
 #include "my.h"
 
-void print_map(char *map, int y, int x, int *ch)
+void print_map(char *map, int y, int x)
 {
     mvprintw(0, 0, "%s", map);
     mvprintw(y, x, "P");
     refresh();
-    *ch = getch();
 }
 
 void checker(int a, int *o_position, char *map)
@@ -29,15 +28,17 @@ void checker(int a, int *o_position, char *map)
 char game_loop(int x, char *map)
 {
     int y = 0, cp = count_p(map), *p_position = position_p(cp, map);
-    int ch = 0, a = count_o(map),  *o_position = position_o(a, map);
-    int r = count_box(map),  *box_position = position_box(r, map);
+    int ch = 0, a = count_o(map), *o_position = position_o(a, map);
+    int r = count_box(map), *box_position = position_box(r, map);
 
     x = find_p(map);
     y = count_nbr_y(y, map);
+    print_map(map, y, x);
     while (1) {
         checker(a, o_position, map);
-        print_map(map, y, x, &ch);
+        ch = getch();
         pos_up(&x, &y, ch, map);
+        print_map(map, y, x);
         if (ch == ' ') {
             put_p_again(cp, p_position, map);
             erase_pre_box(map);
